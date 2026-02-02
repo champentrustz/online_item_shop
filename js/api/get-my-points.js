c2b490b730a8cd5f24841ee2c3a02d05c94023d0
ENTRUSTZ <champ.pittaya.dev@gmail.com>
2026-02-02 19:45:17 +0700

    first commit

--- js/api/get-my-points.js
+++ js/api/get-my-points.js
@@ -4,5 +4,6 @@ const getMyPoints = async() =>{
 
     const res = await fetch(`${apiUrl}/query?token=${myToken}&event=user:getAmount`);
     const data = await res.json();
+    console.log(data);
     return data.response.amount
 }
