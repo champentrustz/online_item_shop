e40233c5fbdc791b3294f057055a00847ea2ed7d
ENTRUSTZ <champ.pittaya.dev@gmail.com>
2026-02-02 19:25:42 +0700

    first commit

--- api/proxy.js
+++ api/proxy.js
@@ -3,7 +3,9 @@ export default async function handler(req, res) {
     const isImage = req.query.type === 'image';
 
     try {
-        const response = await fetch(`http://103.91.190.200:30120/${path}`, {
+        const baseUrl = isImage ? 'http://103.91.190.200:3322' : 'http://103.91.190.200:30120';
+
+        const response = await fetch(`${baseUrl}/${path}`, {
             method: req.method,
             headers: {
                 'Content-Type': 'application/json',
@@ -14,7 +16,7 @@ export default async function handler(req, res) {
         if (isImage) {
             const buffer = await response.arrayBuffer();
             const contentType = response.headers.get('content-type') || 'image/png';
-            const fileName = path.split('/').pop(); // ดึงชื่อไฟล์จาก path
+            const fileName = path.split('/').pop();
 
             res.setHeader('Content-Type', contentType);
             res.setHeader('Content-Disposition', `inline; filename="${fileName}"`);
