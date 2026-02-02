9f6edbe8c5cb500a4afa3b87e9496aaea0388e0a
ENTRUSTZ <champ.pittaya.dev@gmail.com>
2026-02-02 18:52:04 +0700

    first commit

--- js/pages/loading.js
+++ js/pages/loading.js
@@ -13,7 +13,7 @@ const showLoading = (id) =>{
 
     const content = document.getElementById(`${id}`)
 
-    content.innerHTML = `<div class="bg-neutral-950 min-w-screen min-h-full flex justify-center items-center flex-col gap-4">
+    content.innerHTML = `<div class="bg-neutral-950 min-w-full min-h-screen flex justify-center items-center flex-col gap-4">
     <div class="loader"></div>
     <p class="text-gray-400 text-xs">กำลังโหลด...</p>
 </div>`
